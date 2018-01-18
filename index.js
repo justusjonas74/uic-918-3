d2fa5d20b7d0f017707c92048391bc34c3ca4394
justusjonas74 <francis.doege@web.de>
2018-01-18 12:08:43 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const readBarcode = require('./lib/barcode-reader.js');
+exports.readBarcodePNG(file_path, onFulfilled, onRejected);
+
+
+
+//Test
+//const file_path = process.argv[2];
+
+// exports.readBarcodePNG(file_path, (data) => {
+//     const rawBarcode = utils.fixingZXing(data.raw);
+//     const barcode = barcodeData.interpret(rawBarcode);
+//     console.log(util.inspect(barcode, {showHidden: false, depth: null}));
+//   }, (reason) =>{
+//     console.log('ERROR: ' + reason);
+//   }
+// );
