edabd20704c985caec2301dc323f1539556aaf26
justusjonas74 <francis.doege@web.de>
2018-01-22 11:47:16 +0100

    Clean up for publish

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 const readBarcode = require('./lib/barcode-reader.js');
-exports.readBarcodePNG =  (file_path, onFulfilled, onRejected) =>{
+exports.readBarcode =  (file_path, onFulfilled, onRejected) =>{
   readBarcode(file_path, onFulfilled, onRejected);
 };
-
-
-
-//Test
-//const file_path = process.argv[2];
-
-// exports.readBarcodePNG(file_path, (data) => {
-//     const rawBarcode = utils.fixingZXing(data.raw);
-//     const barcode = barcodeData.interpret(rawBarcode);
-//     console.log(util.inspect(barcode, {showHidden: false, depth: null}));
-//   }, (reason) =>{
-//     console.log('ERROR: ' + reason);
-//   }
-// );
