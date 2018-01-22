739cd92f26ebb732d6130ed221cfded686f8b040
justusjonas74 <francis.doege@web.de>
2018-01-22 15:42:16 +0100

    Added function to check if file exists

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const readBarcode = require('./lib/barcode-reader.js');
+const utils = require('./lib/utils.js');
+
 exports.readBarcode =  (file_path, onFulfilled, onRejected) =>{
-  readBarcode(file_path, onFulfilled, onRejected);
+  if (utils.fileExists(file_path)) {
+    readBarcode(file_path, onFulfilled, onRejected);
+  }
 };
