4f9573bbf9862165db7f020d696e579653a5a5d1
justusjonas74 <francis.doege@web.de>
2018-02-08 20:29:03 +0100

    Bugfix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const utils = require('./lib/utils.js');
 exports.readBarcode =  (file_path) =>{
   return new Promise ((resolve, reject) => {
     if (utils.fileExists(file_path)) {
-      return readBarcode(file_path).then((res) => utils.fixingZXing(res)).catch((err) => reject(err));
+      return readBarcode.ZXing(file_path).then((res) => utils.fixingZXing(res)).catch((err) => reject(err));
     } else {
       reject(new Error(`${file_path} not found.`));
     }
