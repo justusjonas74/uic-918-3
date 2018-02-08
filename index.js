f3541e6136e17d210aaae28eadb36e347eefbda9
justusjonas74 <francis.doege@web.de>
2018-02-08 20:20:35 +0100

    Changed to promise style

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const readBarcode = require('./lib/barcode-reader.js');
 const utils = require('./lib/utils.js');
 
-exports.readBarcode =  (file_path, onFulfilled, onRejected) =>{
-  if (utils.fileExists(file_path)) {
-    readBarcode(file_path, onFulfilled, onRejected);
-  }
+exports.readBarcode =  (file_path) =>{
+  return new Promise ((resolve, reject) => {
+    if (utils.fileExists(file_path)) {
+      return readBarcode(file_path).then((res) => utils.fixingZXing(res)).catch((err) => reject(err));
+    } else {
+      reject(new Error(`${file_path} not found.`));
+    }
+  });
+
 };
