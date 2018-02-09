7f7acb2bf80d6573650638b552aca23ea88f786a
justusjonas74 <francis.doege@web.de>
2018-02-09 08:09:06 +0000

    Bugfixes

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const readBarcode = require('./lib/barcode-reader.js');
+const barcode_reader = require('./lib/barcode-reader.js');
 const utils = require('./lib/utils.js');
+const barcodeData = require('./lib/barcode-data.js');
 
-exports.readBarcode =  (file_path) =>{
+function fileWillExists(file_path){
   return new Promise ((resolve, reject) => {
     if (utils.fileExists(file_path)) {
-      return readBarcode.ZXing(file_path).then((res) => utils.fixingZXing(res)).catch((err) => reject(err));
+      resolve(file_path);
     } else {
       reject(new Error(`${file_path} not found.`));
     }
   });
+}
 
+
+let readBarcode = function (file_path){
+   return new Promise ((resolve, reject) => {
+      fileWillExists(file_path)
+        .then((file_path) => barcode_reader.ZXing(file_path))
+        .then((res) => utils.fixingZXing(res.raw))
+        .then((res) => barcodeData.interpret(res))
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
+   });
 };
+
+module.exports = {readBarcode};
