6bf213be9742d6691dfcdf827c1cdd90af677e1f
justusjonas74 <francis.doege@web.de>
2018-02-13 12:40:41 +0000

    Changed a test

--- index.js
+++ index.js
@@ -13,15 +13,31 @@ function fileWillExists(file_path){
 }
 
 
+// let readBarcode = function (file_path){
+//   return new Promise ((resolve, reject) => {
+//       fileWillExists(file_path)
+//         .then((file_path) => barcode_reader.ZXing(file_path))
+//         .then((res) => utils.fixingZXing(res.raw))
+//         .then((res) => barcodeData.interpret(res))
+//         .then((res) => resolve(res))
+//         .catch((err) => reject(err));
+//   });
+// };
+const fixZXING = (res) => {return Promise.resolve(utils.fixingZXing(res.raw))};
+const readZxing = (file_path) => barcode_reader.ZXing(file_path);
+const interpretBarcode = (res) => Promise.resolve(barcodeData.interpret(res));
+
 let readBarcode = function (file_path){
    return new Promise ((resolve, reject) => {
       fileWillExists(file_path)
-        .then((file_path) => barcode_reader.ZXing(file_path))
-        .then((res) => utils.fixingZXing(res.raw))
-        .then((res) => barcodeData.interpret(res))
+        .then(readZxing)
+        .then(fixZXING)
+        .then(interpretBarcode)
         .then((res) => resolve(res))
         .catch((err) => reject(err));
    });
 };
+
+
 
 module.exports = {readBarcode};
--- test/indexTest.js
+++ test/indexTest.js
@@ -8,10 +8,14 @@ var helper = require('./helper');
 
 describe('index.js', () => {
     describe('index.readBarcode', () =>{
-        const dummy = 'test/images/barcode-dummy.png';
+        //const dummy = 'test/images/barcode-dummy.png';
+        const dummy = 'test/images/barcode-dummy2.png';
         const false_dummy = 'test/images/barcode dummy.png';
         it('should return an object on sucess', ()=>{
             main.readBarcode(dummy).should.eventually.be.an('object');
+        });
+        it('should eventually be resolved', ()=>{
+            main.readBarcode(dummy).should.eventually.be.fulfilled;
         });
         it('should reject if file not found', () =>{
             main.readBarcode(false_dummy).should.be.rejected;
