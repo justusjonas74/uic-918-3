febe404f6ead1af93c6822d33e1b8c09fe4f1793
justusjonas74 <francis.doege@web.de>
2018-02-16 14:14:22 +0000

    Beautify code before publishing :)

--- index.js
+++ index.js
@@ -2,29 +2,30 @@ const barcode_reader = require('./lib/barcode-reader.js');
 const utils = require('./lib/utils.js');
 const barcodeData = require('./lib/barcode-data.js');
 
-function fileWillExists(file_path){
-  return new Promise ((resolve, reject) => {
+function fileWillExists(file_path) {
+  return new Promise((resolve, reject) => {
     if (utils.fileExists(file_path)) {
       resolve(file_path);
-    } else {
+    }
+    else {
       reject(new Error(`${file_path} not found.`));
     }
   });
 }
 
-const fixZXING = (res) => {return Promise.resolve(utils.fixingZXing(res.raw))};
+const fixZXING = (res) => { return Promise.resolve(utils.fixingZXing(res.raw)) };
 const readZxing = (file_path) => barcode_reader.ZXing(file_path);
-const interpretBarcode = (res) => {return Promise.resolve(barcodeData.interpret(res))};
+const interpretBarcode = (res) => { return Promise.resolve(barcodeData.interpret(res)) };
 
-let readBarcode = function (file_path){
-   return new Promise ((resolve, reject) => {
-      fileWillExists(file_path)
-        .then(readZxing)
-        .then(fixZXING)
-        .then(interpretBarcode)
-        .then((res) => resolve(res))
-        .catch((err) => reject(err));
-   });
+let readBarcode = function(file_path) {
+  return new Promise((resolve, reject) => {
+    fileWillExists(file_path)
+      .then(readZxing)
+      .then(fixZXING)
+      .then(interpretBarcode)
+      .then((res) => resolve(res))
+      .catch((err) => reject(err));
+  });
 };
 
-module.exports = {readBarcode};
+module.exports = { readBarcode };
