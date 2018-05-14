e8f2be100d11f3b44fc21a7b9d85cdfe7aef3729
justusjonas74 <francis.doege@web.de>
2018-05-14 11:06:49 +0000

    Added support for signature validation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const interpretBarcode = require('./lib/barcode-data.js')
 const fixingZXing = require('./lib/fixingZXing')
 const fileExists = require('./lib/fileExists')
 
+const verifySignature = require('./lib/check_signature').verifyTicket
+
 // TODO: Move this to lib/fileExists.js
 function fileWillExists (filePath) {
   return new Promise((resolve, reject) => {
@@ -18,12 +20,26 @@ const fixZXING = (res) => { return Promise.resolve(fixingZXing(res.raw)) }
 const readZxing = (filePath) => barcodeReader.ZXing(filePath)
 const interpretBarcodeFn = (res) => { return Promise.resolve(interpretBarcode(res)) }
 
-let readBarcode = function (filePath) {
+const checkSignature = async function (ticket, verifyTicket) { 
+  if (verifyTicket) {
+    let isValid = await verifySignature(ticket)
+    ticket.isSignatureValid = isValid 
+  } 
+  return ticket
+}
+
+let readBarcode = function(filePath, options = {}) {
+  let defaults = {
+    verifySignature: false
+  };
+  let opts = Object.assign({}, defaults, options);
+
   return new Promise((resolve, reject) => {
     fileWillExists(filePath)
       .then(readZxing)
       .then(fixZXING)
       .then(interpretBarcodeFn)
+      .then(ticket => checkSignature(ticket, opts.verifySignature))
       .then((res) => resolve(res))
       .catch((err) => reject(err))
   })
---
+++ updateCerts.js
@@ -0,0 +1,1 @@
+require('./lib/get_certs').updateLocalCerts()
