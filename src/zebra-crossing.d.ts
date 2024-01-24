905383eb1a4e48cf2e13793126a3cf35de50c35a
Francis Doege <francis.doege@web.de>
2024-01-24 21:33:38 +0100

    Added Type

--- src/zebra-crossing.d.ts
+++ src/zebra-crossing.d.ts
@@ -1,4 +1,4 @@
-type BarcodeFormats  = 'AZTEC' |'CODABAR' |'CODE_39' |'CODE_93' |'CODE_128' |'DATA_MATRIX' |'EAN_8' |'EAN_13' |'ITF' |'MAXICODE' |'PDF_417' |'QR_CODE' |'RSS_14' |'RSS_EXPANDED' |'UPC_A' |'UPC_E' |'UPC_EAN_EXTENSION'
+type BarcodeFormats = 'AZTEC' | 'CODABAR' | 'CODE_39' | 'CODE_93' | 'CODE_128' | 'DATA_MATRIX' | 'EAN_8' | 'EAN_13' | 'ITF' | 'MAXICODE' | 'PDF_417' | 'QR_CODE' | 'RSS_14' | 'RSS_EXPANDED' | 'UPC_A' | 'UPC_E' | 'UPC_EAN_EXTENSION'
 
 interface ReadingOptions {
     tryHarder?: boolean
@@ -10,9 +10,14 @@ interface ReadingOptions {
 }
 
 // TODO: object type is not recommend
-type ZebraCrossingReturnType = object
+type ZebraCrossingReturnType = {
+    raw: Buffer,
+    parsed: Buffer
+    format: BarcodeFormats,
+    type: string,
+    points: [number, number][],
 
 
 declare module 'zebra-crossing' {
-    function read(file:string|Buffer, options: ReadingOptions) : Promise<ZebraCrossingReturnType>
+    function read(file: string | Buffer, options: ReadingOptions): Promise<ZebraCrossingReturnType>
 }
