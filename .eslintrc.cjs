a2d5cd5816b80ad68c9c32c1ad50e08abe581fe4
Francis Doege <francis.doege@vvo-online.de>
2024-02-19 17:00:52 +0100

    Setup ESLint with Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,14 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-    // 'prettier'
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+    'prettier' ],
   root: true,
   env: {
     browser: false,
@@ -15,6 +18,7 @@ module.exports = {
     node: true
   },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 1
+    '@typescript-eslint/explicit-function-return-type': 1,
+    'prettier/prettier': 2 // Means error
   }
 };
