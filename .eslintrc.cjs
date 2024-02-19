8f8ecb809ac94e4854b432a5114bf9aa166abb44
Francis Doege <francis.doege@vvo-online.de>
2024-02-19 16:26:18 +0100

    Setup ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,20 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      // 'prettier'
+    ],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'jest'],
     root: true,
     env: {
-      browser: true,
+      browser: false,
+      es6: true,
       node: true,
     },
+    rules:{
+      "@typescript-eslint/explicit-function-return-type": 1
+    }
   };
