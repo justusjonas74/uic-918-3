12ed693276f73bbc88a485127692c77d47f32dfc
Justus Jonas <just@Justuss-MacBook-Air.fritz.box>
2025-11-28 20:13:11 +0100

    1.2.0

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,4 @@ export const readBarcode = async function (
 export { default as interpretBarcode } from './barcode-data.js';
 export { parseUFLEX } from './uflex.js';
 export type { UFLEXTicket } from './types/UFLEXTicket.js';
+export type { ParsedUIC918Barcode } from './barcode-data.js';
