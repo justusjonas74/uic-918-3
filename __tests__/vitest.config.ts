4b2a81104aaa9b7ea5b5525c5d72e523b9ff04b4
Francis Doege <francis.doege@web.de>
2026-01-14 13:55:53 +0100

    Fixing Build errors

--- __tests__/vitest.config.ts
+++ __tests__/vitest.config.ts
@@ -1,9 +1,11 @@
 import { configDefaults, coverageConfigDefaults, defineConfig } from 'vitest/config';
 import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
   plugins: [
     wasm(),
+    topLevelAwait()
   ],
   test: {
     exclude: [...configDefaults.exclude, 'build/**/*'],
