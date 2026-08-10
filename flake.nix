5d34014b998568294977a600cc83507e17b53588
Francis Doege <hello@francisdoege.com>
2026-08-10 12:08:04 +0200

    Switched to unstable devenv

--- flake.nix
+++ flake.nix
@@ -2,29 +2,36 @@
   description = "A development environment for uic-918-3";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Unstable
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs_22
+            nodejs
             pnpm
-            # emscripten
-            # asn1c
-            # bash
+            emscripten
+            asn1c
+            bash
           ];
 
           shellHook = ''
-            echo "uic-918-3 development environment loaded"
-            node --version
+            echo "Nix based development environment loaded."
             pnpm --version
+            export PNPM_HOME="$HOME/.local/share/pnpm"
+            export PATH="$PNPM_HOME:$PATH"
           '';
         };
       }
