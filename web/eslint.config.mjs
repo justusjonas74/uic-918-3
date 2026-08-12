4a9b1601f3d00b6a7bf2efda6c75927bd1ecea58
Francis Doege <hello@francisdoege.com>
2026-08-12 16:24:09 +0200

    ci: configure dependabot and build/lint workflows for web subproject

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    }
+  }
 ]);
 
 export default eslintConfig;
